19af465804915c1de48c2e93c9337bb14dc519b2
jessie <jssdnvn@gmail.com>
2021-09-16 13:20:19 -0500

    added photos#new form and added a link to add a photo on trip#show

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,3 +1,17 @@
 class PhotosController < ApplicationController
 
+    def new 
+        @photo = Photo.new
+    end
+
+    
+    def create 
+        @photo = Photo.new(photo_params)
+        if @photo.save 
+            redirect_to photo_path(@photo)
+        else
+            render :new
+        end
+    end
+    
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,5 @@
 class Trip < ApplicationRecord
-    has_many :photos, dependent: :destroy
+    
     scope :with_attached_photos, -> { includes(:photos).merge(Photo.with_attached_file) }
         #Using a join model to annotate Active Storage blobs with custom data
     
