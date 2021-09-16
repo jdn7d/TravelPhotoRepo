af1b89d9976efb54eded762167d5cde930b33a38
jessie <jssdnvn@gmail.com>
2021-09-16 07:44:16 -0500

    finally added private method - user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,4 +16,11 @@ class UsersController < ApplicationController
      def show
         @user = User.find(params[:id] )
      end
+
+     
+   private
+
+   def user_params
+      params.require(:user).permit(:username, :email, :password)
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
 
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+
   resources :users
   resources :photos
   resources :sessions, only: [:create]
